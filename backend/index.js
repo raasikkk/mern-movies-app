fe08e692ad0308cd746d83284e7f40a013dd3a1a
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-12-30 20:35:39 +0500

    genres backend logic added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import path from "path";
 // Files
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import genreRoutes from "./routes/genreRoutes.js";
 
 // Configuration
 dotenv.config();
@@ -23,5 +24,6 @@ const PORT = process.env.PORT || 3000;
 
 // Routes
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/genre", genreRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
